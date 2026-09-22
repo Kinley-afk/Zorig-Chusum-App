a1731999f8ef6e221647ef138fa57303f30180fc
Kinley-afk <kinleynekroz@gmail.com>
2026-09-22 15:03:24 +0600

    Intial commit

---
+++ Zorig Chusum/Zorig_ChusumApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zorig_ChusumApp.swift
+//  Zorig Chusum
+//
+//  Created by iMac15 on 9/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Zorig_ChusumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
